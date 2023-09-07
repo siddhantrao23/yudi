de9fa0f553806ada7d5259426fcde87bd0e293c1
Siddhant Rao <raosiddhant99@gmail.com>
2023-09-07 12:06:13 +0530

    add boilerplate ratatui code

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,36 @@
-use cursive::Cursive;
-use cursive::theme::Palette;
-use cursive::theme::PaletteColor::*;
-use cursive::theme::Color::*;
-use cursive::view::Nameable;
-use cursive::view::Resizable;
-use cursive::views::Dialog;
-use cursive::views::EditView;
-use cursive::views::TextView;
+use yudi::app::{App, AppResult};
+use yudi::event::{Event, EventHandler};
+use yudi::handler::handle_key_events;
+use yudi::tui::Tui;
+use std::io;
+use tui::backend::CrosstermBackend;
+use tui::Terminal;
 
-fn main() {
-    let mut siv = cursive::default();
+fn main() -> AppResult<()> {
+    // Create an application.
+    let mut app = App::new();
 
-    let mut theme = siv.current_theme().clone();
-    
-    let mut p = Palette::default();
-    p[Background] = TerminalDefault;
-    p[View] = TerminalDefault;
-    p[Shadow] = TerminalDefault;
-    p[Primary] = TerminalDefault;
-    p[Secondary] = TerminalDefault;
-    p[Tertiary] = TerminalDefault;
-    p[TitlePrimary] = TerminalDefault;
-    p[Highlight] = TerminalDefault;
-    p[HighlightInactive] = TerminalDefault;
-    
-    theme.palette = p;
-    theme.shadow = false;
-    theme.borders = cursive::theme::BorderStyle::None;
-    siv.set_theme(theme);
-    
-    siv.add_layer(Dialog::new()
-        .title("Thoughts for today....")
-        .padding_lrtb(1, 1, 1, 0)
-        .content(
-            EditView::new()
-                .on_submit(show_popup)
-                .with_name("journal")
-                .fixed_width(20)
-                .fixed_height(20)
-        )
-        .button("Ok", |s| {
-            let entry = s.call_on_name(
-                "journal", |view: &mut EditView| {
-                    view.get_content()
-            })
-            .unwrap();
-            show_popup(s, &entry);
-        })
-    );
+    // Initialize the terminal user interface.
+    let backend = CrosstermBackend::new(io::stderr());
+    let terminal = Terminal::new(backend)?;
+    let events = EventHandler::new(250);
+    let mut tui = Tui::new(terminal, events);
+    tui.init()?;
 
-    siv.add_global_callback('q', |s| s.quit());    
-
-    siv.run();
-}
-
-fn show_popup(s: &mut Cursive, msg: &str) {
-    if msg.is_empty() {
-        s.add_layer(Dialog::info("Please enter journal entry for the day!"));
-    } else {
-        // persist the data
-        s.pop_layer();
-        s.add_layer(
-            Dialog::around(TextView::new(msg))
-                .button("Quit", |s| s.quit())
-        );
+    // Start the main loop.
+    while app.running {
+        // Render the user interface.
+        tui.draw(&mut app)?;
+        // Handle events.
+        match tui.events.next()? {
+            Event::Tick => app.tick(),
+            Event::Key(key_event) => handle_key_events(key_event, &mut app)?,
+            Event::Mouse(_) => {}
+            Event::Resize(_, _) => {}
+        }
     }
+
+    // Exit the user interface.
+    tui.exit()?;
+    Ok(())
 }
